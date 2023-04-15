f24ca5d4efb6db95de3e508e744f5e9339d1ee6c
Chris <mrchrismullins@gmail.com>
2023-04-15 10:49:34 -0400

    Created Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <h1>Welcome to my portfolio shop!</h1>
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Routes>
+            <Route path="/" Component={HomeScreen} />
+            <Route path="/products/:id" Component={ProductScreen} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="light" expand="lg" collapseOnSelect>
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>Portfolio Shop</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>
+                <i className="fas fa-sign-in"></i> Sign In
+              </Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
